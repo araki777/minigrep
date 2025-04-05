ed9fce2c5c4b76d4079e6c7915ec1347c38a4029
araki777 <arakichi1231@gmail.com>
2025-04-05 10:40:16 +0900

    処理をイテレータを利用するように学習

--- src/lib.rs
+++ src/lib.rs
@@ -8,12 +8,18 @@ pub struct Config {
 }
 
 impl Config {
-    pub fn new(args: &[String]) -> Result<Self, &'static str> {
-        if args.len() < 3 {
-            return Err("not enough arguments")
-        }
-        let query: String = args[1].clone();
-        let filename: String = args[2].clone();
+    pub fn new(mut args: impl Iterator<Item = String>) -> Result<Self, &'static str> {
+        args.next();
+
+        let query: String = match args.next() {
+            Some(arg) => arg,
+            None => return Err("Didn't get a query string")
+        };
+
+        let filename: String = match args.next() {
+            Some(arg) => arg,
+            None => return Err("Didn't get a file name")
+        };
 
         Ok(Self { query, filename })
     }
@@ -32,13 +38,18 @@ pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
 }
 
 pub fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
-    let mut results: Vec<&str> = Vec::new();
-    for line in contents.lines() {
-        if line.contains(query) {
-            results.push(line);
-        }
-    }
-    results
+    // let mut results: Vec<&str> = Vec::new();
+    // for line in contents.lines() {
+    //     if line.contains(query) {
+    //         results.push(line);
+    //     }
+    // }
+    // results
+
+    contents
+    .lines()
+    .filter(|line| line.contains(query))
+    .collect()
 }
 
 #[cfg(test)]
--- src/main.rs
+++ src/main.rs
@@ -4,9 +4,9 @@ use std::process;
 use minigrep::Config;
 
 fn main() {
-    let args: Vec<String> = env::args().collect();
+    let args = env::args();
 
-    let config: Config =  Config::new(&args).unwrap_or_else(|err: &str| {
+    let config: Config =  Config::new(args).unwrap_or_else(|err: &str| {
         println!("Problem parsing arguments: {}", err);
         process::exit(1)
     });
